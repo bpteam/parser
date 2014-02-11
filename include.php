9455cea08557ebdf6d7dd1b29eeb01113722ed23
bpteam <bpteam22@gmail.com>
2014-02-11 15:06:18 +0200

    fix include
    many define

--- include.php
+++ include.php
@@ -7,9 +7,8 @@
  * Project: get_content
  * @author: Evgeny Pynykh bpteam22@gmail.com
  */
-define('ROOT_DIR', dirname(__FILE__));
-require_once ROOT_DIR . "/src/cCatalog/cCatalog.php";
-require_once ROOT_DIR . "/src/cGeneratorRegEx/cGeneratorRegEx.php";
-require_once ROOT_DIR . "/src/cPhone/cPhone.php";
-require_once ROOT_DIR . "/src/cRealty/cRealty.php";
-require_once ROOT_DIR . "/src/cWordPress/cWordPress.php";
+require_once dirname(__FILE__) . "/src/cCatalog/cCatalog.php";
+require_once dirname(__FILE__) . "/src/cGeneratorRegEx/cGeneratorRegEx.php";
+require_once dirname(__FILE__) . "/src/cPhone/cPhone.php";
+require_once dirname(__FILE__) . "/src/cRealty/cRealty.php";
+require_once dirname(__FILE__) . "/src/cWordPress/cWordPress.php";
