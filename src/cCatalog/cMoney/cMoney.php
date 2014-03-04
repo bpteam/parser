5caa2f15dedd5e205fc7ca2593e1fe7ef655018e
bpteam <bpteam22@gmail.com>
2014-03-04 16:55:58 +0200

    cMoney pattern

---
+++ src/cCatalog/cMoney/cMoney.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EC
+ * Date: 04.03.14
+ * Time: 16:29
+ * Email: bpteam22@gmail.com
+ */
+
+namespace Parser;
+
+
+class cMoney {
+
+} 
---
+++ src/cCatalog/cMoney/config/ecash.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EC
+ * Date: 04.03.14
+ * Time: 16:31
+ * Email: bpteam22@gmail.com
+ */
+$webMoney = 'w(?:eb)?m(?:oney)?\s*';
+return array(
+	'WMR' => "%{$webMoney}r%i",
+	'WME' => "%{$webMoney}e%i",
+	'WMZ' => "%{$webMoney}z%i",
+	'WMU' => "%{$webMoney}u%i",
+	'WMY' => "%{$webMoney}y%i",
+	'WMB' => "%{$webMoney}b%i",
+	'WMG' => "%{$webMoney}g%i",
+	'WMX' => "%{$webMoney}x%i",
+	'Яндекс.Деньги' => '%(Я(?:нд(?:екс|\.)?)?\s*\.?\s*Д?(?:еньги)|Ya?(?:ndex)?\.?M?(?:oney)?)?%imsu',
+);
