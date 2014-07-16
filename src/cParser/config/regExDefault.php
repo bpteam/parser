3c64d7ff772c5d1fbed643c506d773d915b2d2f2
bpteam <bpteam22@gmail.com>
2014-07-16 09:51:38 +0300

    default regEx

--- src/cParser/config/regExDefault.php
+++ src/cParser/config/regExDefault.php
@@ -2,15 +2,17 @@
 /**
  * Created by PhpStorm.
  * User: EC
- * Date: 15.07.14
- * Time: 10:00
- * Email: bpteam22@gmail.com
+ * Date: 16.07.14
+ * Time: 9:40
+ * Project: parser_carters
+ * @author: Evgeny Pynykh bpteam22@gmail.com
  */
+
 return array(
-	'category' => '',
-	'category_parent' => '',
-	'subcategory' => '',
-	'subcategory_parent' => '',
+	'category' => '%<a\s*href="(?<category_url>[^"]*)">\s*(?<category_name>[^<]*)\s*</a>%imsu',
+	'category_parent' => '%(?<text>.*)%imsu',
+	'subcategory' => '%<a\s*href="(?<subcategory_url>[^"]*)"[^>]*>\s*<span>(?<subcategory_name>[^<]*)</span>%imsu',
+	'subcategory_parent' => '%(?<text>.*)%imsu',
 	'list' => 'regEx for parsing links for ad page index:[unique] - link',
 	'list_parent' => 'parent block for list index: [text] - text for list',
 	'site_page' => 'download page checker index:[0]',
@@ -18,8 +20,6 @@ return array(
 	'current_page' => 'curent page, need for move to the next page index:[current_page] - number of current page',
 	'total_pages' => 'count peges in catalog index:[total_pages]',
 	'pagination_parent' => 'parent block for pagination index:[text]',
-	'ad' => array(
-		'*' => 'regEx of property index:[*]'
-	),
+	'ad' => array(),
 	'ad_parent' => 'parent of ad block index:[text]',
 );
