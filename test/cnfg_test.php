b429bd8d109744f2d841e335474c03da5d91fe27
bpteam <bpteam22@gmail.com>
2014-08-19 14:02:48 +0300

    add test information

--- test/cnfg_test.php
+++ test/cnfg_test.php
@@ -16,25 +16,36 @@ echo "<a href='index.php'>..</a>";
 
 function runTest($functions, $prefix = ''){
 	$start = microtime(true);
-	echo date("[H:i:s Y/m/d]", $start)."\n<br>\n";
+	$memStart = memory_get_usage(true);
+	echo date("[H:i:s Y/m/d]", $start)."[$memStart]\n<br>\n";
 	echo "<table border='1'>";
+	echo "<tr><td>Method</td><td>Result</td><td>time</td><td>memory</td></tr>";
 	foreach($functions as $function){
 		echo "<tr>";
 		echo "<td> $function </td>";
 		$funStart = microtime(true);
+		$memFunStart = memory_get_usage(true);
 		$nameFunction = $prefix.$function;
 		if($nameFunction()){
 			echo "<td> success </td>";
 		} else {
 			echo "<td> <b> ERROR </b> </td>";
-			break;
+			//break;
 		}
 		$funTime = microtime(true) - $funStart;
+		$memFunEnd = memory_get_usage(true) - $memFunStart;
+		gc_collect_cycles();
+		$memFunEndAfterGC = memory_get_usage(true) - $memFunStart;
+		$memoryFunPeak = memory_get_peak_usage(true);
 		echo " <td> $funTime </td>";
+		echo "<td> $memFunEnd ($memFunEndAfterGC)[$memoryFunPeak]</td>";
 		echo "</tr>";
 	}
 	echo "</table>";
+	$memEnd = memory_get_usage(true);
+	$memoryPeak = memory_get_peak_usage(true);
 	$end = microtime(true);
 	echo date('[H:i:s Y/m/d]', $end)."\n<br>\n";
 	echo '[~'.($end-$start).']';
+	echo '['.($memEnd - $memStart).'](' . $memoryPeak .')';
 }
